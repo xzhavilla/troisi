611b96d7e0f2b25c042df6a42e2c32b19cdf23d2
Alberto Villa <alberto.villa@facile.it>
2019-08-03 12:04:14 +0200

    Make prism support unknown types

--- lib/NonEmptyString.ts
+++ lib/NonEmptyString.ts
@@ -1,10 +1,11 @@
+import * as t from 'io-ts';
 import {NonEmptyString} from 'io-ts-types/lib/NonEmptyString';
 import {Prism} from 'monocle-ts';
 
 export const NonEmptyString_ = {
   prism: {
-    self: Prism.fromPredicate<string, NonEmptyString>(
-      (s): s is NonEmptyString => s.length > 0,
+    self: Prism.fromPredicate<unknown, NonEmptyString>(
+      (u): u is NonEmptyString => t.string.is(u) && u.length > 0,
     ),
   },
 };
